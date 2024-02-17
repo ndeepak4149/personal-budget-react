ad17606f11e22959ba08604993f9399c3443d0c9
unknown <you@example.com>
2024-02-17 17:55:41 -0500

    Bought PBudget code in

---
+++ personal-budget-react/src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer classNAme="Bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer> 
+  );
+}
+
+export default Footer;
---
+++ personal-budget-react/src/HomePage/HomePage.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <main NAmeName="center" id="main" role="main">
+
+        <div classNAme="page-area">
+            
+            <section>
+
+            <article>
+                
+                <h2>Stay on track</h2>
+                <p>
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that!
+                </p>
+            </article>
+    
+            <article>
+                <h2>Alerts</h2>
+                <p>
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                </p>
+            </article>
+    
+            <article>
+                <h2>Results</h2>
+                <p>
+                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                    Also, they to live happier lives... since they expend without guilt or fear... 
+                    because they know it is all good and accounted for.
+                </p>
+            </article>
+    
+            <article>
+                <h2>Free</h2>
+                <p>
+                    This app is free!!! And you are the only one holding your data!
+                </p>
+            </article>
+    
+            <article>
+                <h2>Stay on track</h2>
+                <p>
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that!
+                </p>
+            </article>
+    
+            <article>
+                <h2>Alerts</h2>
+                <p>
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                </p>
+            </article>
+    
+    
+            <article>
+                <h2>Chart</h2>
+                <p>
+                    <canvas id="myChart" width="300" height="300" alt="Financial data representation"></canvas>
+                </p>
+            </article>
+            <article>
+                <h2>D3js Chart</h2>
+                <div id="d3jschart"></div>
+            </article>
+        </section>
+        </div>
+
+    </main>
+  );
+}
+
+export default HomePage;
